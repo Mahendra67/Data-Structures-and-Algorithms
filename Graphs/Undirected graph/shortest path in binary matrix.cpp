4568a67a89e965ba110bff525e32c235eb6b464c
Mahendra Majhi <mahendramajhi53@gmail.com>
2022-12-25 04:33:36 +0530

    set approch added

--- Graphs/Undirected graph/shortest path in binary matrix.cpp
+++ Graphs/Undirected graph/shortest path in binary matrix.cpp
@@ -78,6 +78,71 @@ public:
 
 /*---------------------------------------------------------------------------------------------------------------------------------*/
 
+class Solution {
+public:
+    //USING SET 
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        //Edge case : If src node is 1
+        if(grid[0][0] == 1) return -1;
+        
+        int n = grid.size();
+        
+        pair<int,int> src = {0,0};
+        // pair<int,int> des = {n-1,n-1};
+        
+        //Dijktra using SET
+        set<pair<int,pair<int,int>>> s;
+        
+        //min dist vector of vector
+        vector<vector<int>> minCells(n,vector<int> (n,INT_MAX));
+        
+        //Pair : {dist,{row, col}}
+        s.insert({1,{0,0}});
+        //Update minCells of src node to 1
+        minCells[0][0] = 1;
+        
+        while(!s.empty()){
+            auto it = *(s.begin());
+            int dis = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+            s.erase(it);
+            
+            //For its neighbours
+            int possible_row[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+            int possible_col[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+            
+            for(int i=0; i<8; i++){
+                int fr = r + possible_row[i];
+                int fc = c + possible_col[i];
+                
+                //Check if it is valid index
+                if(fr>=0 && fr<n && fc>=0 && fc<n && grid[fr][fc]==0){
+                    //Since path weight between 2 nodes is 1
+                    if(dis + 1 < minCells[fr][fc]){
+                        //Check if the minCells is already relaxed
+                        if(minCells[fr][fc] != INT_MAX){
+                            s.erase({minCells[fr][fc], {fr,fc}});
+                        }
+                        
+                        minCells[fr][fc] = dis + 1;
+                        s.insert({minCells[fr][fc], {fr,fc}});
+                    }
+                }
+            }
+        }
+        
+        //If minCells[n-1][n-1] is unreachable, return -1
+        if(minCells[n-1][n-1] == INT_MAX){
+            return -1;
+        }
+        
+        return minCells[n-1][n-1];
+    }
+};
+
+/*---------------------------------------------------------------------------------------------------------------------------------*/
+
 int main(){
     
     return 0;
