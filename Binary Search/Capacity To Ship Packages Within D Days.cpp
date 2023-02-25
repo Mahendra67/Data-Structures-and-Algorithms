57edb788feac4c85030521be069cef63bba60a12
Mahendra Majhi <mahendramajhi53@gmail.com>
2023-02-25 13:38:54 +0530

    New solutions added

---
+++ Binary Search/Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+#include<bits/stdc++.h>
+//Capacity to Ship Packages withing D Days Leetcode
+
+/*
+A conveyor belt has packages that must be shipped from one port to another within days days.
+
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages 
+on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight 
+capacity of the ship.
+
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being 
+shipped within days days.
+*/
+
+using namespace std;
+
+/*-----------------------------------------------------------------------------------------------------------*/
+
+/*
+TC    :
+SC    :
+*/
+
+/*
+Approach:
+We know that the minimum capacity of the ship should be the max weight of one package.
+(Even if we take less ships/days than no. of packages, the packages will be grouped) -> making the weight of each grouped package larger. Hence, min capacity -> max of weight[i] of pacakges.
+
+//Assuming 6 is the max weight[i]
+Now, we know that least weight/capacity lies in range [6,........]. (we know the lower bound)
+
+Check if there exists an upper bound. (If yes, then usually we can apply Binary search)
+
+The max weight/capacity of a ship can be the total sum of the weights of the package.
+(Assuming all packages are shipped at 1st day).
+
+Now, we know the range of answer(capacity) -> [max(weight[i]), sumOfAllPackages]
+
+Brute force: Start from max(weight[i]) -> one by one, iteratively, check if all the packages can be shipped in given ships/days for the given capacity.
+
+Binary Search: Apply binary search:
+If at mid value of capacity's range, we are able to ship all packages in given days/ships -> that means for all the values greater than mid(capacity) will also be able to ship the packages in the given days.
+e.g.[-----4 5 6 7] -> If it is possible for capacity=4, then the packages can also be shipped
+in greater capacity ships.
+But we need the MINIMUM
+capacity for the ship. Hence, we look in the left half of the range.
+
+If at mid value of capacity's range, we are not able to ship all packages -> means the given capacity is not enough. Also, for lower capacity ship(than the current one) -> it will also be not possible. Hence, look at right half of the range.
+*/
+class Solution {
+public:
+    bool isPossible(int capacity, vector<int>& weights, int days){
+        int temp = capacity;
+        int i = 0;
+        int n = weights.size();
+        
+        //How many ships or days used
+        int ships = 1;
+        
+        while(i<n){
+            temp -= weights[i];
+            
+            if(temp < 0){
+                temp = capacity;
+                ships++;
+                
+                //And put the current package in new ship
+                temp -= weights[i];
+            }
+            
+            i++;
+        }
+        
+        // cout<<capacity<<" "<<ships<<" "<<temp<<endl;
+        //If some capacity is still left or is zero, and if days == ships , then is possible
+        //It is also possible if it takes less no. of days/ships then given days
+        if(temp >= 0 && ships <= days){
+            return true;
+        }
+        return false;
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int maxiWeight = -1e8;
+        for(auto it: weights){
+            maxiWeight = max(maxiWeight, it);
+        }
+        
+        //Now we have the lower bound
+        int lowerBound = maxiWeight;
+        
+        //Find the upper bound (sum of all weights)
+        //This could have been done in upper loop, but for understanding purposes, new loop is used
+        int sum = 0;
+        for(auto &it: weights){
+            sum += it;
+        }
+        
+        int upperBound = sum;
+        
+        //We need to search for minCapacity that the ship needs in the range given
+        int low = lowerBound;
+        int high = upperBound;
+        
+        
+        int MinCap = lowerBound;
+        
+        while(low <= high){
+            //Check if current capacity can take all the packages in given days/ships
+            //mid == current capacity
+            int mid = ((high-low)>>1)+low;
+            
+            if(isPossible(mid, weights, days)){
+                //Check if any lower Capacity ship can do the job
+                high = mid-1;
+                
+                //Update MinCap found so far
+                MinCap = mid;
+            }else{
+                low = mid+1;
+            }
+        }
+        
+        return MinCap;
+    }
+};
+
+/*-----------------------------------------------------------------------------------------------------------*/
---
+++ PQ and Heaps/Minimize deviation in an array.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<bits/stdc++.h>
+//Minimize deviation in an array Leetcode 1675
+
+/*
+You are given an array nums of n positive integers.
+
+You can perform two types of operations on any element of the array any number of times:
+
+If the element is even, divide it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the last element, and the array will be [1,2,3,2].
+If the element is odd, multiply it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the first element, and the array will be [2,2,3,4].
+The deviation of the array is the maximum difference between any two elements in the array.
+
+Return the minimum deviation the array can have after performing some number of operations.
+*/
+
+using namespace std;
+
+/*-----------------------------------------------------------------------------------------------------------*/
+
+/*
+TC    :
+SC    :
+*/
+
+/*
+We can multiply odd number by 2, and can divide even number by 2.
+Observation:
+Mulitplying odd number by 2 once will make it an even number.
+
+Multiply all the odd numbers present in the array.
+And if the lower bound was odd, this will maximize the lower bound.
+But this will also, increase the upper bound -> which we do not need as we are looking for minimum deviation (deviation: diff between min number and max number in the array).
+
+Also, after every operation(*by 2 and / by 2), we need the array to be in sorted order everytime we perform some operation -> as we need to find the deviation. -> Use priority queue.
+
+
+Now, the max element in array has to be even. Find the deviation and perform 'divide by 2' operation on it. Before pushing it back to the pq, check if it has become the min number of the array.
+e.g.
+min num = 3 //current
+[3,4]  -> 4/2 = 2 -> [2,4]
+
+new min num = 2 -> now this will be used to find deviation [upperBound - 2]
+
+Now, the max element of the array has 2 cases:
+1. It is odd: You can't perform any operation on it. (As if you multipy it by 2, it will only increase the deviation ->  but we need min deviation.
+Hence, if upperBound becomes odd, it will give minDeviation.
+
+2. It is even: Same operation from line 13-16. Keep doing this until you find odd number as UpperBound.
+*/
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        int ans = 1e9;
+        int mini = 1e9;
+        
+        int n = nums.size();
+        
+        priority_queue<int> pq;
+        for(int i=0; i<n; i++){
+            //If it is odd, multiply it by 2
+            nums[i] = (nums[i]&1)? nums[i]*2 : nums[i];
+            pq.push(nums[i]);
+            
+            //Also, since we are using PQ, we need to keep track of min value of array
+            mini = min(mini, nums[i]); 
+        }
+        
+        
+        //Now, while the upperBound is even
+        while(!(pq.top()&1)){
+            int upperBound = pq.top();
+            pq.pop();
+            
+            //Find the deviation
+            ans = min(ans, upperBound - mini);
+            
+            //Since, it is even -> we can perform divide by 2
+            int newNum = upperBound/2;
+            //Check if it has become the min number in the pq
+            mini = min(mini, newNum);
+            
+            //Push back the new Num
+            pq.push(newNum);
+        }
+        
+        //If the upperBound becomes odd -> can't perform * by 2 -> will only increase the deviation
+        //Hence if we find odd number as upperBound -> it will give minDeviation
+        
+        ans = min(ans, pq.top() - mini);
+        
+        return ans;
+    }
+};
+
+/*-----------------------------------------------------------------------------------------------------------*/
